912a037b5441f08376941b2aebdb7a30480953be
meddeb17 <mohamedoussema.meddeb@esprit.tn>
2025-12-04 01:49:37 +0100

    interfaces de meddeb

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./index.html",
+      "./pages/**/*.{js,ts,jsx,tsx,html}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
